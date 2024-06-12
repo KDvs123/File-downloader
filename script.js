4c9d2b7c66f5393bfa592e669c70b2a0e6b9f49c
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-06-13 01:08:52 +0530

    change the button text to "Downloading File..." during downloading and add the catch method to call if any error appears during download

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-const fileInput=document.querySelector("input"),
-downloadBtn=document.querySelector("button");
+const fileInput = document.querySelector("input"),
+  downloadBtn = document.querySelector("button");
 
-downloadBtn.addEventListener("click", e =>{
-    e.preventDefault();//preventing form from submitting
-    fetchFile(fileInput.value);
+downloadBtn.addEventListener("click", (e) => {
+  e.preventDefault(); //preventing form from submitting
+  downloadBtn.innerText = "Downloading file.....";
+  fetchFile(fileInput.value);
+});
 
+function fetchFile(url) {
+  //fetching file and returning response as blob
 
-})
-
-function fetchFile(url){
-    //fetching file and returning response as blob
-
-    fetch(url).then(res=>res.blob().then(file=>{
+  fetch(url)
+    .then((res) =>
+      res.blob().then((file) => {
         // URL.createObjURL creates a url of passed object
-        let tempUrl=URL.createObjectURL(file);
-        let aTag=document.createElement("a");
-        aTag.href=tempUrl;// passing tempUrl as href value of <a> tag
-        aTag.download=url.replace(/^.*[\\\/]/, '');//passing file last name & extention as download value of <a> tag
-        document.body.appendChild(aTag);//adding <a> tag inside body
-        aTag.click();//clicking <a> tag so the file downlaod
-        aTag.remove();//removing <a> tag once file downloaded
+        let tempUrl = URL.createObjectURL(file);
+        let aTag = document.createElement("a");
+        aTag.href = tempUrl; // passing tempUrl as href value of <a> tag
+        aTag.download = url.replace(/^.*[\\\/]/, ""); //passing file last name & extention as download value of <a> tag
+        document.body.appendChild(aTag); //adding <a> tag inside body
+        aTag.click(); //clicking <a> tag so the file downlaod
+        aTag.remove(); //removing <a> tag once file downloaded
         URL.revokeObjectURL(tempUrl);
-    }))
-
+        downloadBtn.innerText = "Download File";
+      })
+    )
+    .catch(() => {
+      //catch method will call if any error comes during downloading
+      downloadBtn.innerText = "Download File";
+      alert("Failed to Download File!");
+    });
 }
-
